df8941af74e8b55d436e637c1becdd747d924190
2607077992ZQ <2607077992@qq.com>
2024-11-10 21:53:56 +0800

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,9 +1,19 @@
 @echo off
 chcp 65001
-update\AutoUpdate.exe -i 0 -s 2607077992ZQ/ProjectTest/main -t Github -v 1.0.0
+
+update\AutoUpdate.exe -i 0 -s 2607077992ZQ/ProjectTest/main -t Github
 update\AutoUpdate.exe -r start.bat
+
+echo 当前版本1.0.0
+update\AutoUpdate.exe -v 1.0.0
+
 echo 正在检查自动更新
 update\AutoUpdate.exe -a 0
-echo 更新完成
-project\hello.txt
+
+if %errorlevel%==0 (
+	echo 已更新至1.0.1
+	update\AutoUpdate.exe -v 1.0.1
+	start project\hello.txt
+)
+
 pause
